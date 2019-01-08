8d46fb79d44ff24309fa6d497fc51abe51b5a8de
Kotvitz <kotvitz17@gmail.com>
2019-01-08 11:36:18 +0100

    hand-made BACK button was replaced with the system button

--- app/src/main/java/com/example/kotvitz/michalkotfica/SplashScreenActivity.java
+++ app/src/main/java/com/example/kotvitz/michalkotfica/SplashScreenActivity.java
@@ -9,19 +9,13 @@ import android.widget.Button;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    final Handler handler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
-        final Button button = findViewById(R.id.button);
         final Intent mainScreen = new Intent(this, MainActivity.class);
-        final Handler handler = new Handler();
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                handler.removeMessages(0);
-            }
-        });
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -31,4 +25,8 @@ public class SplashScreenActivity extends AppCompatActivity {
         }, 5000);
     }
 
+    @Override
+    public void onBackPressed() {
+        handler.removeMessages(0);
+    }
 }
